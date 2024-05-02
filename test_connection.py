9d1a8814ac7afe27fced10d5ac58f35817d870ee
surtecha <suryatejachalla.dev@gmail.com>
2024-05-02 11:39:42 +0530

    test: Connection success

---
+++ test_connection.py
@@ -0,0 +1,15 @@
+from pyfirmata import Arduino, util
+import time
+
+board = Arduino('/dev/cu.usbmodem21401')    # Change the port to the one you are using
+
+print(board.get_firmata_version())
+
+blinkCount = input("Blink count: ")
+
+for i in range(int(blinkCount)):
+    print("Blink counter: " + str(i+1))
+    board.digital[13].write(1)
+    time.sleep(0.5)
+    board.digital[13].write(0)
+    time.sleep(0.5)
